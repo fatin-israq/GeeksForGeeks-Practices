fa0c99a272fb85732224a9c305bf09930126a314
Fatin Israq Talha <israqq2120@gmail.com>
2023-09-21 02:28:26 +0600

    Catagorized folders

---
+++ Arrays/Reverse_a_String.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int size = str.length();
+
+        for (int i = 0, j = size - 1; i < size/2; i++, j--){
+            char tempChar = str[i];
+            str[i] = str[j];
+            str[j] = tempChar;
+        }
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
